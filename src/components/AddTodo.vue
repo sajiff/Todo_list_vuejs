c5207624b2b575da78e1c19f7a25d1a5f1ecc590
sajid <anindasajid@gmail.com>
2021-01-09 00:15:52 +0600

    fix 1

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -9,6 +9,7 @@
 </template>
 
 <script>
+import { v4 as uuidv4 } from 'uuid'; 
 export default {
     name: "AddTodo",
     data(){
@@ -20,6 +21,7 @@ export default {
         addTodo(e) {
             e.preventDefault();
             const newTodo = {
+                id: uuidv4(),
                 title: this.title,
                 completed: false
             }
